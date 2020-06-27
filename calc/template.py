ec00c6a7982f340073a08bfbfa8f99f51d349252
seiyoon <yoony0193@kookmin.ac.kr>
2020-06-28 04:19:04 +0900

    issue #4-3 Modify template.py

--- calc/template.py
+++ calc/template.py
@@ -3,18 +3,7 @@ html = b"""
     <body>
 	<form method="get" action="">
 	    <p>
-		SUM: <input type="text" name="sum" value="%(sum)s">
-	    </p>
-	    <p>
-		PRODUCT: <input type="text" name="product" value="%(product)s">
-	    </p>
-	    <p>
-		sum = <input type="number" name="a">
-		    + <input type="number" name="b">
-	    </p>
-	    <p>
-		product = <input type="number" name="a">
-		        * <input type="number" name="b">
+		input two numbers: <input type="number" name="a"> <input type="number" name"b">
 	    </p>
 	    <p>
 		<input type="submit" value="Submit">
@@ -27,3 +16,32 @@ html = b"""
     </body>
 </html>
 """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
